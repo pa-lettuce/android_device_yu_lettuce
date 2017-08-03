50a0ab5ca4ecbb84f4d4f666291c150424f04d67
dev-harsh1998 <harshitjain6751@gmail.com>
2017-08-03 16:40:15 +0530

    lettuce: fix camera paths for AOSP builds

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -52,9 +52,9 @@ LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-display)/libqdutils \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
+        hardware/qcom/display/libgralloc \
+        hardware/qcom/display/libqdutils \
+        hardware/qcom/media/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
@@ -70,12 +70,12 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libqservice
+        hardware/qcom/display/libqservice
 
 #ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
 #LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
 #else
-LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
 #endif
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
